5821061aeb9d7a0b74977af0d618c10eeb92be9d
Radoslaw Palka <radekpalka@gmail..com>
2021-12-11 12:51:58 +0100

    add math operation

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
-const numBtns = document.querySelectorAll("button.number")
-const input= document.querySelector("p.input")
-const addNumber = el =>{
-  const textNumber= el.path[0].textContent
-  if (input.textContent === "0"){
-  input.textContent = textNumber
-  }
-  else{
-    input.textContent += textNumber
-  }
-}
+const numBtns = document.querySelectorAll('button.number');
+const input = document.querySelector('p.input');
+const operationBtns = document.querySelectorAll('button.operation');
+const pAct = document.querySelector('p.act');
+let number1;
+let number2;
+let sign;
+const addNumber = (el) => {
+	const number = el.path[0].textContent;
+	if (input.textContent.length < 8) {
+		if (input.textContent === '0' || sign) {
+			input.textContent = number;
+		} else {
+			input.textContent += number;
+		}
+	}
+};
 
-numBtns.forEach(btn => btn.addEventListener("click", addNumber))
+const operation = (el) => {
+	const number = input.textContent;
+	if (!number1) {
+		number1 = number;
+	} else {
+		number2 = number;
+	}
+	
+	if (number2) {
+		if (sign === '+') {
+			number1 = (parseFloat(number1) + parseFloat(number2)).toString();
+		} else if (sign === '-') {
+			number1 = (parseFloat(number1) - parseFloat(number2)).toString();
+		} else if (sign === '*') {
+			number1 = (parseFloat(number1) * parseFloat(number2)).toString();
+		} else if (sign === '/') {
+			number1 = (parseFloat(number1) / parseFloat(number2)).toString();
+		}
+	}
+  sign = el.path[0].textContent;
+  console.log(sign)
+	pAct.textContent = `${number1} ${sign}`;
+};
+
+operationBtns.forEach((btn) => btn.addEventListener('click', operation));
+numBtns.forEach((btn) => btn.addEventListener('click', addNumber));
